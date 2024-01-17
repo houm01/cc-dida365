1d7f7cb971c3bf9fdedcf8428ee3449388ca367a
Your Name <you@example.com>
2024-01-17 08:18:03 +0800

    Released v0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 setup(
     name='cc-dida365',  # package name
